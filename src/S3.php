71da87d9390d8c49e9b2a365a31eb2323a5a27c3
leo <leo@bm-sms.com.tw>
2014-04-05 12:39:53 +0800

    added S3 storage class

---
+++ src/S3.php
@@ -0,0 +1,153 @@
+<?php
+
+use Aws\S3\S3Client;
+
+class S3 implements StorageInterface
+{
+	/**
+	 * S3 client
+	 *
+	 * @var  S3Client
+	 */
+	protected $client;
+
+	/**
+	 * Configuration
+	 *
+	 * @var  array
+	 */
+	protected $config;
+
+	/**
+	 * Constructor
+	 *
+	 * @param   array  $config  Configurations:
+	 *                          - key: API KEY
+	 *                          - secret: API SECRET
+	 *                          - bucket: S3 bucket name
+	 *                          - region: S3 bucket region
+	 *                          - hash: (optional) True to enable filename hash, false to disable.
+	 *                          		Default is false
+	 *
+	 * @throws  Exception
+	 */
+	public function __construct(array $config)
+	{
+		$this->setConfig($config);
+
+		// Create S3Client instance
+		$this->client = S3Client::factory(array(
+			'key'    => $config['key'],
+			'secret' => $config['secret'],
+			'bucket' => $config['bucket'],
+			'region' => $config['region'],
+		));
+	}
+
+	/**
+	 * Put file to S3
+	 *
+	 * @param   string  $localFilePath   Local file path
+	 * @param   string  $remoteFilePath  S3 file path
+	 *
+	 * @return  \Guzzle\Service\Resource\Model
+	 */
+	public function put($localFilePath, $remoteFilePath)
+	{
+		// Hash file name
+		if ($this->config['hash'])
+		{
+			$dir = dirname($remoteFilePath);
+			$ext = pathinfo($localFilePath, PATHINFO_EXTENSION);
+
+			$remoteFilePath = $dir . '/' . sha1_file($localFilePath) . '.' . $ext;
+		}
+
+		$result = $this->client->putObject(array(
+			'ACL' => 'public-read',
+			'Bucket' => $this->config['bucket'],
+			'Key' => $remoteFilePath,
+			'SourceFile' => $localFilePath,
+		));
+
+		// We can poll the object until it is accessible
+		$this->client->waitUntilObjectExists(array(
+			'Bucket' => $this->config['bucket'],
+			'Key'    => $remoteFilePath,
+		));
+
+		return $result;
+	}
+
+	/**
+	 * Delete S3 file
+	 *
+	 * @param   string  $remoteFilePath  S3 file path
+	 *
+	 * @return  \Guzzle\Service\Resource\Model
+	 */
+	public function delete($remoteFilePath)
+	{
+		$result = $this->client->deleteObject(array(
+			'Bucket' => $this->config['bucket'],
+			'Key' => $remoteFilePath,
+		));
+
+		return $result;
+	}
+
+	/**
+	 * Get configuration
+	 *
+	 * @return  array
+	 */
+	public function getConfig()
+	{
+		return $this->config;
+	}
+
+	/**
+	 * Setup configuration
+	 *
+	 * @param   array  $config
+	 *
+	 * @return  void
+	 *
+	 * @throws  Exception
+	 */
+	public function setConfig(array $config)
+	{
+		// Check required configuration
+		foreach (array('key', 'secret', 'bucket', 'region') as $name)
+		{
+			if (! isset($config[$name]))
+			{
+				throw new Exception(sprintf('configuration %s is not exist.', $name));
+			}
+		}
+
+		$config['hash'] = empty($config['hash']) ? false : (bool) $config['hash'];
+
+		$this->config = $config;
+	}
+
+	/**
+	 * Enable file hash
+	 *
+	 * @return  void
+	 */
+	public function enableHash()
+	{
+		$this->config['hash'] = true;
+	}
+
+	/**
+	 * Disable file hash
+	 *
+	 * @return  void
+	 */
+	public function disableHash()
+	{
+		$this->config['hash'] = false;
+	}
+}
---
+++ src/StorageInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+interface StorageInterface
+{
+	/**
+	 * Put file to storage
+	 *
+	 * @param   string  $localFilePath   Local file path
+	 * @param   string  $storageFilePath  Storage file path
+	 *
+	 * @return  mixed
+	 */
+	public function put($localFilePath, $storageFilePath);
+
+	/**
+	 * Delete storage file
+	 *
+	 * @param   string  $storageFilePath  Storage file path
+	 *
+	 * @return  mixed
+	 */
+	public function delete($storageFilePath);
+}
